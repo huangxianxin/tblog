5539ce55137f1e38c83a86264a10537e0537649a
ShrCheng <shichengoooo@sina.com>
2016-02-22 14:58:38 +0800

    修改config.js数据库配置
    修改数据库名为tblog

--- configs/config.js
+++ configs/config.js
@@ -1,6 +1,6 @@
 module.exports = { 
 	cookieSecret: 'blog_Cookie', // Cookie 加密
-	db: '111111', // 数据库的名称
+	db: 'tblog', // 数据库的名称
 	host: 'localhost', // 数据库的地址
 	port: 27017, // 数据库的端口号
 	safe: true // 数据库连接模式
